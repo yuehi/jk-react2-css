4c8030f3d8283f95b785bf4b51fa8db676aa1684
YUHEI <76258455+yuehi@users.noreply.github.com>
2021-11-22 12:31:49 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/Styled.jsx";
+import { StyledComponents } from "./components/StyledComponents";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +9,8 @@ export default function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
+      <StyledComponents />
     </div>
   );
 }
